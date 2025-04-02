ee2b571c4e43fa388ea615a0e8e34475c8ccdeef
Ephraim200608 <philipephraim2006@gmail.com>
2025-04-02 12:35:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const resumeButton = document.querySelector("button");
+    resumeButton.addEventListener("click", function () {
+        alert("Not available currently");
+    });
+});
